4c06c6214ff89cb856a6bed12c34f736a2d14132
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-01 12:36:28 -0500

    DATA VARIABLES passing through for title category and city and rare so far

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -6,7 +6,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-list.component.scss']
 })
 export class RentalListComponent implements OnInit {
-  rentals: any[] = [1,2,3,4];
+  rentals: any[] = [{
+    id: 1,
+    title: "Central Apartment",
+    city: "New York",
+    street: "Times Square",
+    category: "apartment",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 2,
+    title: "Central Apartment",
+    city: "Dallas",
+    street: "San Antonio Street",
+    category: "Castle",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 3,
+    title: "Central Apartment",
+    city: "San Francisco",
+    street: "Main Street",
+    category: "Mansion",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 4,
+    title: "Central Apartment",
+    city: "Bratslavia",
+    street: "Hlavna",
+    category: "Castle",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 5,
+    title: "Central Apartment",
+    city: "Berlin",
+    street: "Haupt Strasse",
+    category: "apartment",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 6,
+    title: "Central Apartment",
+    city: "Montserat",
+    street: "Baglva Road",
+    category: "House",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 7,
+    title: "Central Apartment",
+    city: "Houston",
+    street: "Rainey Street",
+    category: "House",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 8,
+    title: "Central Apartment",
+    city: "Austin",
+    street: "6th Street",
+    category: "apartment",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  },
+  {
+    id: 9,
+    title: "Central Apartment",
+    city: "Chicago",
+    street: "Michigan Ave",
+    category: "Condo",
+    image: "http://viaplaceholder.com/350x250",
+    description: "Room Fit for a King",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "11/2/2019"
+  }];
 
   constructor() { }
 
