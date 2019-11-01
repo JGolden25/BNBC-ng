27eb161a42944462ad5706c2b003d83d3a483440
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-01 11:37:39 -0500

    NGFOR loop for rentals listing

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-list.component.scss']
 })
 export class RentalListComponent implements OnInit {
+  rentals: any[] = [1,2,3,4];
 
   constructor() { }
 
