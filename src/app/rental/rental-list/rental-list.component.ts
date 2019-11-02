cf036498bc74cb9c1c18fc404f526c61b36a7127
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-01 20:13:48 -0500

    DEBUGGER included with timeouts for loading data

--- src/app/rental/rental-list/rental-list.component.ts
+++ src/app/rental/rental-list/rental-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RentalService } from '../shared/rental.service';
 
 @Component({
   selector: 'bwm-rental-list',
@@ -6,118 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rental-list.component.scss']
 })
 export class RentalListComponent implements OnInit {
-  rentals: any[] = [{
-    id: 1,
-    title: "Central Apartment",
-    city: "New York",
-    street: "Times Square",
-    category: "apartment",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 2,
-    title: "Central Apartment",
-    city: "Dallas",
-    street: "San Antonio Street",
-    category: "Castle",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 3,
-    title: "Central Apartment",
-    city: "San Francisco",
-    street: "Main Street",
-    category: "Mansion",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 4,
-    title: "Central Apartment",
-    city: "Bratslavia",
-    street: "Hlavna",
-    category: "Castle",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 5,
-    title: "Central Apartment",
-    city: "Berlin",
-    street: "Haupt Strasse",
-    category: "apartment",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 6,
-    title: "Central Apartment",
-    city: "Montserat",
-    street: "Baglva Road",
-    category: "House",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 7,
-    title: "Central Apartment",
-    city: "Houston",
-    street: "Rainey Street",
-    category: "House",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 8,
-    title: "Central Apartment",
-    city: "Austin",
-    street: "6th Street",
-    category: "apartment",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  },
-  {
-    id: 9,
-    title: "Central Apartment",
-    city: "Chicago",
-    street: "Michigan Ave",
-    category: "Condo",
-    image: "http://viaplaceholder.com/350x250",
-    description: "Room Fit for a King",
-    dailyRate: 34,
-    shared: false,
-    createdAt: "11/2/2019"
-  }];
-
-  constructor() { }
+  rentals: any[] = [];
+  constructor(private rentalService: RentalService) { }
 
   ngOnInit() {
+    debugger;
+    const rentalObservable = this.rentalService.getRentals();
+
+    debugger;
+    rentalObservable.subscribe(
+      (rentals) => {
+        debugger;
+        this.rentals = rentals;
+      },
+      (err) => {
+        debugger;
+      },
+      () => {
+        debugger;
+      });
   }
 
 }
--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -4,13 +4,16 @@ import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { CommonModule } from '@angular/common';
 
+import { RentalService } from './shared/rental.service';
+
 @NgModule({
     declarations: [
         RentalListComponent,
         RentalListItemComponent,
         RentalComponent
     ],
-    imports: [CommonModule]
+    imports: [CommonModule],
+    providers: [RentalService]
 })
 
 export class RentalModule {}
