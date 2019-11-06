179101f78395209c08372beafaff0e6fece2db42
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-06 01:10:00 -0600

    map not working

--- src/app/common/map/map.component.ts
+++ src/app/common/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'bwm-map',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   
+  @Input() location: string;
+
   lat = 51.678418;
   lng = 7.809007;
 
