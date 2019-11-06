845749773ab6102a0792f8c693053417d8a84309
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-06 00:58:27 -0600

    MAP display works

--- src/app/common/map/map.component.ts
+++ src/app/common/map/map.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
+  
+  lat = 51.678418;
+  lng = 7.809007;
 
   constructor() { }
 
